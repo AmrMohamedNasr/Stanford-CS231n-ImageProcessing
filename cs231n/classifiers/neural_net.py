50673ac2decccc0d6c15744233399a2275aa0d02
Michael <micoraafat96@gmail.com>
2018-12-16 21:16:22 +0200

    begin NN

--- cs231n/classifiers/neural_net.py
+++ cs231n/classifiers/neural_net.py
@@ -75,7 +75,8 @@ class TwoLayerNet(object):
     # Store the result in the scores variable, which should be an array of      #
     # shape (N, C).                                                             #
     #############################################################################
-    pass
+    layer= np.sum(X.dot(W1), b1)
+    scores = np.sum(layer.dot(W2), b2)
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
